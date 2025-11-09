fca7e37878b049e951129f5dc0d961391ce74024
Aman Mohd <amanmohdtp@gmail.com>
2025-11-09 22:42:12 +0530

    🍭

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -1,1 +1,22 @@
-..
+const siteConfig = {
+  companyName: "Your Company Name",
+  logo: "/logo.png",
+  videoUrl: "https://example.com/video.mp4",
+  aboutText: `We are a company dedicated to innovation and excellence.\nOur mission is to deliver top-quality solutions to our clients worldwide.`,
+  brochure: {
+    name: "Company Brochure",
+    fileUrl: "/brochure.pdf",
+  },
+  contact: {
+    email: "info@company.com",
+    whatsapp: "+911234567890",
+    address: "123 Business Street, City, Country",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/",
+      facebook: "https://facebook.com/",
+      twitter: "https://twitter.com/",
+    },
+  },
+};
+export default siteConfig;
